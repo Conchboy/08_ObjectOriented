2121e50e18e624c42ca4ff19d40234e268929bfe
conchboy <panchaocn@hotmail.com>
2021-01-30 21:53:34 +0800

    updated from laptop

--- rp_09_小明爱跑步.py
+++ rp_09_小明爱跑步.py
@@ -9,10 +9,12 @@ class Person:
         return "我的名字叫 %s 我的体重是 %.2f " % (self.name, self.weight)
 
     def run(self):
-        pass
+        print(" %s 爱跑步,跑步锻炼身体" % self.name)
+        self.weight -= 0.5
 
     def eat(self):
-        pass
+        print(" %s 爱吃鸡腿,吃鸡腿让人心情舒畅" % self.name)
+        self.weight += 0.75
 
 
 xiaoming = Person("小明", 75)
