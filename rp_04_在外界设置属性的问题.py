ead5b42755c21703f5ca1ed91f8d7b98770c7158
conchboy <panchaocn@hotmail.com>
2021-01-30 16:47:08 +0800

    updated from laptop

--- rp_04_在外界设置属性的问题.py
+++ rp_04_在外界设置属性的问题.py
@@ -1,5 +1,6 @@
 class Cat:
     def eat(self):
+        # 那一个对象调用的方法, self就是那一个对象的引用
         print("小猫%s爱吃鱼" % self.name)
 
     def drink(self):
@@ -8,19 +9,8 @@ class Cat:
 
 tom = Cat()
 # 对象的属性在类定义外定义, 不推荐
-tom.name = "Tom"
-
-lazy_cat = Cat()
-lazy_cat.name = "大懒猫"
-
-lazy_cat2 = lazy_cat
+# tom.name = "Tom"
 
 tom.eat()
 tom.drink()
-lazy_cat.eat()
-lazy_cat.drink()
-print(tom)
-print(lazy_cat)
-print(lazy_cat2)
-addr = id(tom)
-print("%x" % addr)
+tom.name = "Tom"
