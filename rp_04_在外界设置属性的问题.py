a2aa9cdf3fd5f7f78d7b1ef5f93c5cc472d3531d
conchboy <panchaocn@hotmail.com>
2021-01-30 16:51:00 +0800

    updated from laptop

--- rp_04_在外界设置属性的问题.py
+++ rp_04_在外界设置属性的问题.py
@@ -1,4 +1,8 @@
 class Cat:
+    def __init__(self, name):
+        print("这是一个初始化方法")
+        # self.属性名 = 属性的初始值
+        self.name = name
     def eat(self):
         # 那一个对象调用的方法, self就是那一个对象的引用
         print("小猫%s爱吃鱼" % self.name)
@@ -7,10 +11,10 @@ class Cat:
         print("小猫%s爱喝水" % self.name)
 
 
-tom = Cat()
+tom = Cat("Tom")
 # 对象的属性在类定义外定义, 不推荐
 # tom.name = "Tom"
 
 tom.eat()
 tom.drink()
-tom.name = "Tom"
+# tom.name = "Tom"
