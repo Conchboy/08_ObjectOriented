847801075d3ae47df4a9c90f9dd1bd34f514f748
conchboy <panchaocn@hotmail.com>
2021-01-31 12:20:43 +0800

    updated from laptop

--- rp_17_私有属性和私有方法.py
+++ rp_17_私有属性和私有方法.py
@@ -2,14 +2,16 @@ class Women:
     def __init__(self, name):
 
         self.name = name
-        self.age = 18
+        self.__age = 18
 
-    def __age(self):
-        pass
-
-    def __secrete(self):
-        print(" %s的年龄是 %d" % (self.name, self.age))
+    def secret(self):
+        # 在对象的方法内部可以访问对象的私有属性
+        print("%s的年龄是 %d" % (self.name, self.__age))
 
 
 xiaofang = Women("小芳")
-print(xiaofang.age)
+# 私有属性在外部不能直接访问
+# print(xiaofang.__age)
+# 私有方法同样不能在方法外部调用
+xiaofang.secret()
+
