9b0c1ed2e41e33e689e49e58af62e100dc2256c6
conchboy <panchaocn@hotmail.com>
2021-01-31 12:11:49 +0800

    updated from laptop

---
+++ rp_17_私有属性和私有方法.py
@@ -0,0 +1,11 @@
+class Women:
+    def __init__(self, name):
+
+        self.name = name
+        self.age = 18
+
+    def __age(self):
+        pass
+
+    def __secrete(self):
+        print(" %s的年龄是 %d" % (self.name, self.age))
